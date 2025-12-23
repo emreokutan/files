eeb98bd7d198d5cffc5fbfbbc959a1d282be722b
emreokutan <emre.okutan@gmail.com>
2025-12-23 13:54:32 +0300

    Update custom-scripts.js

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,17 +1,3 @@
-    (function () {
-        var options = {
-            whatsapp: "905426955574", // WhatsApp numarası
-            call_to_action: "Bizimle iletişime geçin!", // Görüntülenecek yazı
-            position: "left", // Sağ taraf için 'right' sol taraf için 'left'
-        };
-        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-    })();
-</script>
-
-<script>
 
 $(document).ready(function(){
 
@@ -795,3 +781,4 @@ $(document).on('DOMNodeInserted', function() {
   $('[id^="gb-widget"]').css('display', 'block');
 }
 });
+
